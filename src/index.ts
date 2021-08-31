65c2cc365559a33a318840458b6dcbd5870e4ad4
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-08-31 00:27:50 -0300

    Preparando o index.ts para que este seja substituido  pela classe Lista de Tarefa.

--- src/index.ts
+++ src/index.ts
@@ -3,47 +3,11 @@ import "./style.css";
 import "material-icons/iconfont/material-icons.css";
 import { Tarefa, Prioridade } from "./models/Tarefa";
 
-// Seletores
+const tarefas: Tarefa[] = []
+
 let tabela = document.getElementById("table");
 let input = document.querySelector("input");
 let formulario = document.querySelector("form");
-
-// -- OBJETOS --
-
-// -- ARRAY TAREFAS --
-const tarefas: Tarefa[] = []
-
-// Escutando eventos
-formulario.addEventListener("submit", adicionarTarefa);
-
-// Funções
-
-function adicionarTarefa(evento) {
-    // interrompe envio padrão
-    evento.preventDefault(); 
-
-    // limpa array, p/ evitar redundância
-    while (tarefas.length > 0) {
-        // remove primeiro elemento 
-        tarefas.shift();
-    }
-
-    // evita que o input vazio seja enviado
-    if (! (input.value == "")) {
-    
-        // criando nova tarefa
-        const novaTarefa = new Tarefa(input.value, Prioridade.alta);
-    
-        // add no array cada tarefa
-        tarefas.push(novaTarefa);
-    
-        // chama a função com a tarefa recém criada
-        exibirTarefa(tarefas);
-
-        // limpa input após tarefa ser enviada
-        input.value = "";
-    };
-}
 
 function exibirTarefa(tarefas:Tarefa[]) { 
 
@@ -52,4 +16,20 @@ function exibirTarefa(tarefas:Tarefa[]) {
     }
 }
 
-// Consoles log
+formulario.addEventListener("submit", (evento) => {
+    evento.preventDefault(); 
+
+    while (tarefas.length > 0) {
+        tarefas.shift();
+    }
+
+    if (input.value == "") return;
+    
+    const novaTarefa = new Tarefa(input.value, Prioridade.alta);
+
+    tarefas.push(novaTarefa);
+
+    tabela.appendChild(novaTarefa.toRow());
+
+    input.value = "";
+});
